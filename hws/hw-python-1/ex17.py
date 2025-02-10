98295cfe65c4091f006794ece7f091172d04c414
Niyaz <Niyaz>
2025-02-11 00:23:18 +0500

    update

--- hws/hw-python-1/ex17.py
+++
@@ -1,8 +0,0 @@
-def char_count(string, char):
-    print(string.count(char))
-
-string = 'happy new year!'
-char = "h"
-char_count(string,char)
-
-
--- hws/hw-python-1/ex3.py
+++
@@ -1,4 +0,0 @@
-def string_len (input_string):
-    return len(input_string)
-print(string_len('nfactorial'))
-
